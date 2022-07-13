3539f7d4aa8311ea9f3d6c3b25e5394129846f00
Louis Nguyen <103094279+louis-nguyen108@users.noreply.github.com>
2022-07-13 17:32:45 +1200

    initial setup auth0 authentication

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,24 +1,70 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-
-import { fetchFruits } from '../actions'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
-  const fruits = useSelector((state) => state.fruits)
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(fetchFruits())
-  }, [])
+  // const dispatch = useDispatch()
+  useEffect(() => {}, [])
+
+  const { logout, loginWithRedirect } = useAuth0()
+
+  function handleRegister(e) {
+    e.preventDefault()
+    return loginWithRedirect({
+      redirectUri: `${window.location.origin}/register`,
+    })
+  }
+
+  const { isAuthenticated, user } = useAuth0()
+
+  // function handleRegister(e) {
+  //   e.preventDefault()
+  //   loginWithRedirect({
+  //     redirectUri: `${window.location.origin}/register`,
+  //   }).then((result) => {
+  //     console.log(result)
+  //   })
+  // }
+
+  function handleLogin(e) {
+    e.preventDefault()
+    const result = loginWithRedirect()
+    console.log(result)
+  }
+
+  function handleLogout(e) {
+    e.preventDefault()
+    const result = logout()
+    console.log(result)
+  }
+
+  function consoleLog(e) {
+    e.preventDefault()
+    console.log('isAuthenticated', isAuthenticated)
+    console.log('user', user)
+  }
 
   return (
     <>
       <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
+        {/* <h1>Fullstack Boilerplate - with Fruits!</h1>
         <ul>
           {fruits.map((fruit) => (
             <li key={fruit}>{fruit}</li>
           ))}
-        </ul>
+        </ul> */}
+        {/* receipts_keeper@hotmail.com
+receiptsTest!23 */}
+        <h1>Hello World</h1>
+        <IfAuthenticated>
+          <button onClick={handleLogout}>Log out</button>
+          <button onClick={consoleLog}>Console Log</button>
+        </IfAuthenticated>
+        <IfNotAuthenticated>
+          <button onClick={handleRegister}>Register</button>
+          <button onClick={handleLogin}>Log in</button>
+        </IfNotAuthenticated>
       </div>
     </>
   )

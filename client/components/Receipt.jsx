4447cc72aea1eb50aa1dff3c1a828dc3bfb513c6
lauren_kong_mako_22 <103288573+lauren-kong@users.noreply.github.com>
2022-07-14 11:45:11 +1200

    small changes

--- client/components/Receipt.jsx
+++ client/components/Receipt.jsx
@@ -1,5 +1,45 @@
+import { Favorite, MoreVert, Share } from '@mui/icons-material'
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  IconButton,
+} from '@mui/material'
 import React from 'react'
+import { useEffect } from 'react'
 
-export default function Receipt() {
-  return <div>Receipt</div>
+export default function Receipt(props) {
+  const { receipt } = props
+  useEffect(() => {
+    console.log(receipt)
+  }, [])
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader title={receipt.name} subheader={receipt.purchase_date} />
+      <CardMedia
+        component="img"
+        height="194"
+        image={receipt.image}
+        alt={receipt.name}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {receipt.note}
+        </Typography>
+        {receipt.expiryDate ? (
+          <Typography variant="body2" color="text.secondary">
+            Warranty expired on{receipt.expiryDate}
+          </Typography>
+        ) : (
+          <Typography variant="body2" color="text.secondary">
+            No Warranty for the purchase
+          </Typography>
+        )}
+      </CardContent>
+    </Card>
+  )
 }
--- client/components/Receipts.jsx
+++ client/components/Receipts.jsx
@@ -37,10 +37,42 @@ export default function Receipts() {
       warrantyId: 2,
       expiryDate: new Date('2024', '4', '11').toLocaleDateString('en-NZ'),
     },
+    {
+      id: 3,
+      auth0Id: 'auth0|62ce51224e478f1e65cfb444',
+      userName: 'Lauren',
+      name: 'Toaster',
+      image:
+        'https://thumbs.dreamstime.com/b/vector-paper-check-sell-receipt-bill-template-vector-paper-cash-sell-receipt-139437685.jpg',
+      purchase_date: new Date('2022', '6', '11').toLocaleDateString('en-NZ'),
+      store: 'Noel Leeming',
+      price: 108.5,
+      categoryId: 1,
+      note: 'bought a toaster for office',
+      categoryType: 'appliance',
+      warrantyId: 3,
+      expiryDate: new Date('2023', '6', '11').toLocaleDateString('en-NZ'),
+    },
+    {
+      id: 4,
+      auth0Id: 'auth0|62ce51224e478f1e65cfb444',
+      userName: 'Lauren',
+      name: 'Hair dryer',
+      image:
+        'https://thumbs.dreamstime.com/b/vector-paper-check-sell-receipt-bill-template-vector-paper-cash-sell-receipt-139437685.jpg',
+      purchase_date: new Date('2022', '4', '11').toLocaleDateString('en-NZ'),
+      store: 'Harvey Norman',
+      price: 65.99,
+      categoryId: 1,
+      note: undefined,
+      categoryType: 'appliance',
+      warrantyId: 4,
+      expiryDate: new Date('2024', '4', '11').toLocaleDateString('en-NZ'),
+    },
   ]
   return (
     <Box bgcolor="pink" flex={5} p={2}>
-      <Stack direction="row" col={3}>
+      <Stack direction="row" col={3} gap={10}>
         {receipts?.map((receipt, index) => {
           return <Receipt key={index} receipt={receipt} />
         })}

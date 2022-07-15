2c4b088c24d8da9d142b4bd0909b7fc066685337
Louis Nguyen <103094279+louis-nguyen108@users.noreply.github.com>
2022-07-15 16:17:24 +1200

    initial setup actions and reducers for Receipts and Stores

--- client/apis/receipts.js
+++ client/apis/receipts.js
@@ -2,19 +2,22 @@ import request from 'superagent'
 
 const receiptsUrl = '/api/v1/receipts'
 
-export function getReceipts() {
-  return request.get(receiptsUrl).then((res) => res.body)
+export function getReceipts(token) {
+  return request
+    .get(receiptsUrl)
+    .set('authorization', `Bearer ${token}`)
+    .then((res) => res.body)
 }
 
-export function addReceipt(receipt, token) {
+export function postReceipt(receipt, token) {
   return request
     .post(receiptsUrl)
-    .set('authorization', `Bearer ${token}`) // is this required?
+    .set('authorization', `Bearer ${token}`)
     .send(receipt)
     .then((res) => res.body)
 }
 
-export function updateReceipt(receipt, token) {
+export function patchReceipt(receipt, token) {
   return request
     .patch(receiptsUrl)
     .set('authorization', `Bearer ${token}`)
@@ -28,12 +31,4 @@ export function deleteReceipt(receipt, token) {
     .set('authorization', `Bearer ${token}`)
     .send(receipt)
     .then((res) => res.body)
-}
-
-export function getStores(token) {
-  return request
-    .get(`${receiptsUrl}/all/stores`)
-    .set('authorization', `Bearer ${token}`)
-    .then((res) => res.body)
-    .catch(logError)
 }

ec1369fc1158459b30f291a4ee8bd0dd3f076e47
lauren_kong_mako_22 <103288573+lauren-kong@users.noreply.github.com>
2022-07-15 16:56:55 +1200

    commit prior to pull

--- client/components/AddReceiptForm.jsx
+++ client/components/AddReceiptForm.jsx
@@ -20,7 +20,7 @@ import {
 } from '@mui/material'
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
-import { PhotoCamera } from '@mui/icons-material'
+import { PhotoCamera, ReceiptLong } from '@mui/icons-material'
 
 const categories = [
   'Books',
@@ -66,43 +66,22 @@ export default function AddReceiptForm({ modalState, close }) {
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Stack direction="row">
-        {preview && (
-          <Box
-            sx={{
-              '& .MuiTextField-root': { my: 0.5, width: '100%' },
-              '& > :not(style)': { my: 0.5, width: '100%' },
-              width: { xs: '250px', sm: '400px', md: '400px' },
-              height: { xs: '500px', sm: '600px', md: '600px' },
-            }}
-            bgcolor="white"
-            p={3}
-            borderRadius={5}
-          >
-            <img
-              style={{
-                display: 'block',
-              }}
-              src={preview}
-            />
-          </Box>
-        )}
-
-        <Box
-          component="form"
-          sx={{
-            '& .MuiTextField-root': { my: 0.5, width: '100%' },
-            '& > :not(style)': { my: 0.5, width: '100%' },
-            width: { xs: '250px', sm: '400px', md: '400px' },
-            height: { xs: '500px', sm: '600px', md: '600px' },
-          }}
-          bgcolor="white"
-          p={3}
-          borderRadius={5}
-        >
-          <Typography variant="h6" color="primary" textAlign="center">
-            New Receipt
-          </Typography>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { my: 0.5, width: '100%' },
+          '& > :not(style)': { my: 0.5, width: '100%' },
+          width: { xs: '250px', sm: '400px', md: '400px' },
+          height: { xs: '500px', sm: '600px', md: '600px' },
+        }}
+        bgcolor="white"
+        p={3}
+        borderRadius={5}
+      >
+        <Typography variant="h6" color="primary" textAlign="center">
+          New Receipt
+        </Typography>
+        {!preview && (
           <IconButton color="primary" component="label" sx={{ width: '50px' }}>
             <input
               hidden
@@ -112,118 +91,123 @@ export default function AddReceiptForm({ modalState, close }) {
             />
             <PhotoCamera />
           </IconButton>
-          {/* Receipt Name */}
-          <TextField
-            id="receipt-name"
-            label="Name"
-            variant="outlined"
-            // value={receipt.name}
+        )}
+        {preview && (
+          <IconButton color="primary" component="label" sx={{ width: '50px' }}>
+            <ReceiptLong />
+            Preview
+          </IconButton>
+        )}
+
+        {/* Receipt Name */}
+        <TextField
+          id="receipt-name"
+          label="Name"
+          variant="outlined"
+          // value={receipt.name}
+          // onChange={handleChange}
+        />
+
+        {/* Price */}
+        <FormControl fullWidth>
+          <InputLabel htmlFor="outlined-adornment-amount">Price</InputLabel>
+          <OutlinedInput
+            id="price"
+            label="Price"
+            // value={receipt.price}
             // onChange={handleChange}
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
           />
-
-          {/* Price */}
-          <FormControl fullWidth>
-            <InputLabel htmlFor="outlined-adornment-amount">Price</InputLabel>
-            <OutlinedInput
-              id="price"
-              label="Price"
-              // value={receipt.price}
-              // onChange={handleChange}
-              startAdornment={
-                <InputAdornment position="start">$</InputAdornment>
-              }
-            />
-          </FormControl>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <DatePicker
-              disableFuture
-              label="Purchase Date"
-              openTo="year"
-              views={['year', 'month', 'day']}
-              inputFormat="dd/MM/yyyy"
-              value={purchaseDate}
-              onChange={(newDate) => {
-                setPurchaseDate(newDate)
-              }}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
-
-          {/* Category */}
-          <TextField
-            id="category"
-            label="Category"
-            select
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          >
-            {categories.map((category) => (
-              <MenuItem key={category} value={category}>
-                {category}
-              </MenuItem>
-            ))}
-          </TextField>
-
-          {/* Note */}
-          <TextField
-            id="note"
-            label="Note"
-            multiline
-            rows={2}
-            placeholder="Enter your note here..."
-            // value={receipt.note}
-            // onChange={handleChange}
+        </FormControl>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DatePicker
+            disableFuture
+            label="Purchase Date"
+            openTo="year"
+            views={['year', 'month', 'day']}
+            inputFormat="dd/MM/yyyy"
+            value={purchaseDate}
+            onChange={(newDate) => {
+              setPurchaseDate(newDate)
+            }}
+            renderInput={(params) => <TextField {...params} />}
           />
+        </LocalizationProvider>
 
-          {/* Warranty */}
-          <FormGroup sx={{ alignItems: 'center' }}>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={warrantyChecked}
-                  onChange={(e) => {
-                    setWarrantyChecked(!warrantyChecked)
-                  }}
-                />
-              }
-              label="Warranty"
-            />
-          </FormGroup>
+        {/* Category */}
+        <TextField
+          id="category"
+          label="Category"
+          select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          {categories.map((category) => (
+            <MenuItem key={category} value={category}>
+              {category}
+            </MenuItem>
+          ))}
+        </TextField>
 
-          {/* Warranty Period */}
-          {warrantyChecked && (
-            <Grid container>
-              <Grid item xs={8}>
-                <TextField id="warranty-duration" label="Warranty" />
-              </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  id="warranty-period"
-                  label=""
-                  select
-                  value={period}
-                  onChange={(e) => setPeriod(e.target.value)}
-                >
-                  {periods.map((period) => (
-                    <MenuItem key={period} value={period}>
-                      {period}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </Grid>
+        {/* Note */}
+        <TextField
+          id="note"
+          label="Note"
+          multiline
+          rows={2}
+          placeholder="Enter your note here..."
+          // value={receipt.note}
+          // onChange={handleChange}
+        />
+
+        {/* Warranty */}
+        <FormGroup sx={{ alignItems: 'center' }}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={warrantyChecked}
+                onChange={(e) => {
+                  setWarrantyChecked(!warrantyChecked)
+                }}
+              />
+            }
+            label="Warranty"
+          />
+        </FormGroup>
+
+        {/* Warranty Period */}
+        {warrantyChecked && (
+          <Grid container>
+            <Grid item xs={8}>
+              <TextField id="warranty-duration" label="Warranty" />
             </Grid>
-          )}
+            <Grid item xs={4}>
+              <TextField
+                id="warranty-period"
+                label=""
+                select
+                value={period}
+                onChange={(e) => setPeriod(e.target.value)}
+              >
+                {periods.map((period) => (
+                  <MenuItem key={period} value={period}>
+                    {period}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+          </Grid>
+        )}
 
-          {/* Add Button */}
-          <Button
-            variant="contained"
-            type="submit"
-            // onClick={handleSubmit}
-          >
-            Add
-          </Button>
-        </Box>
-      </Stack>
+        {/* Add Button */}
+        <Button
+          variant="contained"
+          type="submit"
+          // onClick={handleSubmit}
+        >
+          Add
+        </Button>
+      </Box>
     </StyledModal>
   )
 }

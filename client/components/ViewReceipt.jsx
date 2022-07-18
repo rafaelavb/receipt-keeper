4b4ac8b79788bbabec20fd3d37b053dd722a30a0
lauren_kong_mako_22 <103288573+lauren-kong@users.noreply.github.com>
2022-07-18 19:45:25 +1200

    added confirmation box when user click delete button

--- client/components/ViewReceipt.jsx
+++ client/components/ViewReceipt.jsx
@@ -11,6 +11,11 @@ import {
   ListItem,
   ListItemText,
   Divider,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
 } from '@mui/material'
 import { Close, ReceiptLong } from '@mui/icons-material'
 
@@ -26,6 +31,12 @@ export default function ViewReceipt({
   const token = useSelector((state) => state.loggedInUser.token)
   const dispatch = useDispatch()
   const [viewImage, setViewImage] = useState(false)
+  const [openConfirm, setOpenConfirm] = useState(false)
+
+  function handleOpenConfirm(e) {
+    setOpenConfirm(true)
+  }
+
   function setViewMode(e) {
     e.preventDefault()
     setViewImage(!viewImage)
@@ -240,7 +251,7 @@ export default function ViewReceipt({
             <Button variant="contained" onClick={handleClickOpen}>
               Edit
             </Button>
-            <Button variant="contained" onClick={handleDelete}>
+            <Button variant="contained" onClick={handleOpenConfirm}>
               Delete
             </Button>
           </Stack>
@@ -252,6 +263,25 @@ export default function ViewReceipt({
         close={handleClose}
         closeView={close}
       />
+      <Dialog
+        open={openConfirm}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{'Delete Receipt'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete this receipt?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={(e) => setOpenConfirm(false)}>Cancel</Button>
+          <Button onClick={handleDelete} autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   )
 }

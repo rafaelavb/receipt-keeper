52495c799679070e34ad5a679d9187a03abe9295
Louis Nguyen <103094279+louis-nguyen108@users.noreply.github.com>
2022-07-20 15:08:16 +1200

    add fakeReceiptToDelete to fake-data, complete test for deleteReceipt db function

--- server/db/tests/receipts.test.js
+++ server/db/tests/receipts.test.js
@@ -7,6 +7,7 @@ const {
   fakePostReceiptWithWarranty,
   fakePatchReceipt,
   fakePatchedReceipt,
+  fakeReceiptToDelete,
 } = require('../../../tests/fake-data')
 
 beforeAll(() => {
@@ -22,87 +23,79 @@ afterAll(() => {
 })
 
 describe('getReceipts', () => {
-  it('returns a list of receipts by user ID (auth0Id)', () => {
+  it('returns a list of receipts by user ID (auth0 ID)', async () => {
     expect.assertions(8)
 
-    return db.getReceipts('auth0|random', testDb).then((actual) => {
-      expect(actual).toHaveLength(4)
-      expect(actual[0].id).toBe(1)
-      expect(actual[1].price).toBe(2300)
-      expect(actual[2].warrantyId).toBe(3)
-      expect(actual[2].expiryDate).toBeNull()
-      expect(actual[2].warrantyPeriod).toBeNull()
-      expect(actual[3].categoryType).toBe('Spyware')
-      expect(actual[3].warrantyPeriodUnit).not.toBeNull()
-    })
+    const actual = await db.getReceipts('auth0|random', testDb)
+
+    expect(actual).toHaveLength(4)
+    expect(actual[0].id).toBe(1)
+    expect(actual[1].price).toBe(2300)
+    expect(actual[2].warrantyId).toBe(3)
+    expect(actual[2].expiryDate).toBeNull()
+    expect(actual[2].warrantyPeriod).toBeNull()
+    expect(actual[3].categoryType).toBe('Spyware')
+    expect(actual[3].warrantyPeriodUnit).not.toBeNull()
   })
 })
 
 describe('getReceipt', () => {
-  it('returns a single receipt by receipt ID', () => {
+  it('returns a single receipt by receipt ID', async () => {
     expect.assertions(5)
 
-    return db.getReceipt(3, testDb).then((actual) => {
-      expect(actual.id).toBe(3)
-      expect(actual.name).toBe('Microwave')
-      expect(actual.purchaseDate).toBe('10/06/2021')
-      expect(actual.categoryType).toBe('Hardware')
-      expect(actual.note).toBe('Expensive microwave')
-    })
+    const actual = await db.getReceipt(3, testDb)
+
+    expect(actual.id).toBe(3)
+    expect(actual.name).toBe('Microwave')
+    expect(actual.purchaseDate).toBe('10/06/2021')
+    expect(actual.categoryType).toBe('Hardware')
+    expect(actual.note).toBe('Expensive microwave')
   })
 })
 
 describe('addReceipt', () => {
-  it('posts a single receipt to the db', () => {
+  it('adds a single receipt to the db', async () => {
     expect.assertions(5)
 
-    return db
-      .addReceipt('auth0|random', fakePostReceiptWithWarranty, testDb)
-      .then(() => {
-        // eslint-disable-next-line promise/no-nesting
-        return testDb('receipts')
-          .select()
-          .then((receipts) => {
-            const actual = receipts[receipts.length - 1]
+    await db.addReceipt('auth0|random', fakePostReceiptWithWarranty, testDb)
+    const receipts = await testDb('receipts').select()
+    const actual = receipts[receipts.length - 1]
 
-            expect(receipts).toHaveLength(5)
-            expect(actual.auth0_id).toBe('auth0|random')
-            expect(actual.name).toEqual(fakePostReceiptWithWarranty.name)
-            expect(actual.image).toEqual(fakePostReceiptWithWarranty.image)
-            expect(actual.purchase_date).toEqual(
-              fakePostReceiptWithWarranty.purchaseDate
-            )
-          })
-      })
+    expect(receipts).toHaveLength(5)
+    expect(actual.auth0_id).toBe('auth0|random')
+    expect(actual.name).toEqual(fakePostReceiptWithWarranty.name)
+    expect(actual.image).toEqual(fakePostReceiptWithWarranty.image)
+    expect(actual.purchase_date).toEqual(
+      fakePostReceiptWithWarranty.purchaseDate
+    )
   })
 })
 
 describe('updateReceipt', () => {
-  it('update a receipt by user ID (auth0Id) and receipt ID', () => {
-    // expect.assertions(1)
+  it('update a receipt by user ID (auth0 ID) and receipt ID', async () => {
+    expect.assertions(5)
 
-    return db
-      .updateReceipt('auth0|random', fakePatchReceipt, testDb)
-      .then(() => {
-        // eslint-disable-next-line promise/no-nesting
-        return testDb('receipts')
-          .select()
-          .where({ id: fakePatchReceipt.id })
-          .first()
-          .then((receipt) => {
-            const actual = receipt
+    await db.updateReceipt('auth0|random', fakePatchReceipt, testDb)
+    const actual = await testDb('receipts')
+      .select()
+      .where({ id: fakePatchReceipt.id })
+      .first()
 
-            expect(actual.name).toEqual(fakePatchedReceipt.name)
-            expect(actual.store).toEqual(fakePatchedReceipt.store)
-            expect(actual.purchase_date).toEqual(
-              fakePatchedReceipt.purchaseDate
-            )
-            expect(actual.price).toEqual(fakePatchedReceipt.price)
-            expect(actual.note).toEqual(fakePatchedReceipt.note)
-          })
-      })
+    expect(actual.name).toEqual(fakePatchedReceipt.name)
+    expect(actual.store).toEqual(fakePatchedReceipt.store)
+    expect(actual.purchase_date).toEqual(fakePatchedReceipt.purchaseDate)
+    expect(actual.price).toEqual(fakePatchedReceipt.price)
+    expect(actual.note).toEqual(fakePatchedReceipt.note)
   })
 })
 
-// *** TO DO ***
-describe('deleteReceipt', () => {})
+describe('deleteReceipt', () => {
+  it('deletes a receipt by user ID (auth0 ID) and receipt ID', async () => {
+    expect.assertions(1)
+
+    await db.deleteReceipt('auth0|random', fakeReceiptToDelete, testDb)
+    const actual = await testDb('receipts').select()
+
+    expect(actual).toHaveLength(3)
+  })
+})
--- tests/fake-data.js
+++ tests/fake-data.js
@@ -239,6 +239,8 @@ export const fakePatchReceipt = {
 
 export const fakePatchedReceipt = fakePatchReceipt
 
+export const fakeReceiptToDelete = fakeClientReceipts[1]
+
 export function FakeLinkedPage() {
   const { store } = useParams()
   return <div>{store}</div>

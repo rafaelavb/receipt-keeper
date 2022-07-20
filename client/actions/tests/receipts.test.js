c57b1e90375dc8b44710637647732f4b981c7bfe
Rafael <103238522+rafaelavb@users.noreply.github.com>
2022-07-20 16:29:54 +1200

    finished receipts action tests

--- client/actions/tests/receipts.test.js
+++ client/actions/tests/receipts.test.js
@@ -57,40 +57,38 @@ describe('requestReceipts', () => {
   })
 })
 
-// patchReceipt.mockReturnValue(Promise.resolve(fakeClientReceipts))
+beforeEach(() => {
+  jest.clearAllMocks()
+})
 
-// beforeEach(() => {
-//   jest.clearAllMocks()
-// })
+describe('patchReceipts', () => {
+  it('dispatches', async () => {
+    expect.assertions(2)
+    const updatedReceipt = { value: 'updated' }
+    patchReceipt.mockReturnValue(Promise.resolve(updatedReceipt))
+    const thunkFn = updateReceipt(1)
+    await thunkFn(fakeDispatch)
 
-// describe('patchReceipts', () => {
-//   it('dispatches', async () => {
-//     expect.assertions(3)
-//     const thunkFn = updateReceipt(1)
-//     await thunkFn(fakeDispatch)
+    //     // Act
+    const firstAction = fakeDispatch.mock.calls[0][0]
+    // const secondAction = fakeDispatch.mock.calls[0][1]
 
-//     //     // Act
-//     const firstAction = fakeDispatch.mock.calls[0][0]
-//     const secondAction = fakeDispatch.mock.calls[1][0]
+    //     // Assertion
+    expect(firstAction.type).toEqual(UPDATE_RECEIPT)
+    expect(firstAction.payload).toEqual(updatedReceipt)
+  })
 
-//     //     // Assertion
-//     expect(firstAction.type).toEqual(UPDATE_RECEIPT)
-//     expect(secondAction.type).toEqual(RECEIVE_RECEIPTS)
-//     expect(secondAction.payload).toEqual(fakeClientReceipts)
-//   })
-
-//   it('dispatches error when api call fails', () => {
-//     getReceipts.mockImplementation(() => Promise.reject(new Error('uh oh!')))
-//     expect.assertions(3)
-//     return updateReceipt(1)(fakeDispatch).finally(() => {
-//       const firstAction = fakeDispatch.mock.calls[0][0]
-//       const secondAction = fakeDispatch.mock.calls[1][0]
-//       expect(firstAction.type).toEqual(UPDATE_RECEIPT)
-//       expect(secondAction.type).toEqual(RECEIPTS_ERROR)
-//       expect(secondAction.error).toContain('uh oh!')
-//     })
-//   })
-// })
+  it('dispatches error when api call fails', () => {
+    patchReceipt.mockImplementation(() => Promise.reject(new Error('uh oh!')))
+    expect.assertions(2)
+    return updateReceipt()(fakeDispatch).finally(() => {
+      const firstAction = fakeDispatch.mock.calls[0][0]
+      console.log(`firstAction`, firstAction)
+      expect(firstAction.type).toEqual(RECEIPTS_ERROR)
+      expect(firstAction.error).toContain('uh oh!')
+    })
+  })
+})
 
 postReceipt.mockReturnValue(
   Promise.resolve([...fakeClientReceipts, { value: 'adding receipt' }])
@@ -141,13 +139,10 @@ describe('deleteReceipt', () => {
 
     //     // Act
     const firstAction = fakeDispatch.mock.calls[0][0]
-    console.log(`im the first action`, firstAction)
-    // const secondAction = fakeDispatch.mock.calls[0][1]
 
     // Assertion
 
     expect(firstAction.type).toEqual(DELETE_RECEIPT)
-    console.log(firstAction.type)
     expect(firstAction.payload).toHaveLength(3)
   })
 

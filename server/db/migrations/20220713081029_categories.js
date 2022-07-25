a626ec417f3f8c09615beea955f6c4a3db2e0288
lauren_kong_mako_22 <103288573+lauren-kong@users.noreply.github.com>
2022-07-25 23:27:12 +1200

    commit prior to deployment

--- server/db/migrations/20220713081029_categories.js
+++ server/db/migrations/20220713081029_categories.js
@@ -4,7 +4,7 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable('categories', (table) => {
-    table.increments('id')
+    table.increments('id').primary()
     table.string('type')
   })
 }
